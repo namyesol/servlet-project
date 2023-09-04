af0f2a2891da4a2a56ebca264580e19b659b0c6a
namyesol <artnam20@gmail.com>
2023-09-04 17:15:10 +0900

    불필요한 import 문 제거

--- src/main/java/com/controller/community/CommunityListServlet.java
+++ src/main/java/com/controller/community/CommunityListServlet.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.common.Constants;
 import com.common.Page;
 import com.common.PageResponseDTO;
--- src/main/java/com/controller/notice/NoticeListServlet.java
+++ src/main/java/com/controller/notice/NoticeListServlet.java
@@ -1,7 +1,6 @@
 package com.controller.notice;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
